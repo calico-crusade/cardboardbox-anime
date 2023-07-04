f8c345d22767df0ad531c250663de3fce60df9a7
calico-crusade <holycardboardquest@gmail.com>
2023-07-04 16:22:45 -0400

    Resizing images that bloat epub size

--- src/CardboardBox.ImageTransformers/Extensions.cs
+++ src/CardboardBox.ImageTransformers/Extensions.cs
@@ -4,11 +4,23 @@ namespace CardboardBox.ImageTransformers;
 
 public static class Extensions
 {
-    public static async Task ConvertImage(this Stream input, Stream output, MagickFormat format = MagickFormat.Png, int quality = 75)
+    public static async Task ConvertImage(this Stream input, Stream output, MagickFormat format = MagickFormat.Png, int quality = 75, int maxSize = 1024)
     {
         using var mgc = new MagickImage(input);
+
+        if (mgc.Width > maxSize || mgc.Height > maxSize)
+        {
+            var ratio = new[] 
+            { 
+                (double)maxSize / mgc.Width, 
+                (double)maxSize / mgc.Height 
+            }.Min();
+
+            mgc.Resize(new Percentage(ratio * 100));
+        }
+        
         mgc.Format = format;
-        mgc.Quality = 75;
+        mgc.Quality = quality;
         await mgc.WriteAsync(output);
     }
 }
