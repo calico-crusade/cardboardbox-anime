10f589e5c4bf5265dd8eee08b103594efb133b1c
calico-crusade <holycardboardquest@gmail.com>
2023-08-17 23:29:11 -0400

    Fixing issue with saucenao

--- src/CardboardBox.Match/SauceNaoApiService.cs
+++ src/CardboardBox.Match/SauceNaoApiService.cs
@@ -1,6 +1,7 @@
 ﻿using CardboardBox.Http;
 using CardboardBox.Match.SauceNao;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System.Net;
 
 namespace CardboardBox.Match;
@@ -16,15 +17,18 @@ public class SauceNaoApiService : ISauceNaoApiService
 {
 	private readonly IConfiguration _config;
 	private readonly IApiService _api;
+	private readonly ILogger _logger;
 
-	public string ApiKey => _config["SauceNao:Token"];
+	public string ApiKey => _config["SauceNao:Token"] ?? throw new ArgumentNullException(nameof(ApiKey));
 
 	public SauceNaoApiService(
 		IConfiguration config,
-		IApiService api)
+		IApiService api,
+		ILogger<SauceNaoApiService> logger)
 	{
 		_config = config;
 		_api = api;
+		_logger = logger;
 	}
 
 	public Task<Sauce?> Get(string imageUrl, params SauceNaoDatabase[] databases)
@@ -32,22 +36,30 @@ public class SauceNaoApiService : ISauceNaoApiService
 		return Get(imageUrl, 6, databases);
 	}
 
-	public Task<Sauce?> Get(string imageUrl, int resultCount, params SauceNaoDatabase[] databases)
+	public async Task<Sauce?> Get(string imageUrl, int resultCount, params SauceNaoDatabase[] databases)
 	{
-		var pars = new Dictionary<string, string>
+		try
 		{
-			["url"] = WebUtility.UrlEncode(imageUrl),
-			["output_type"] = "2",
-			["api_key"] = ApiKey,
-			["numres"] = resultCount.ToString(),
-		};
+			var pars = new Dictionary<string, string>
+			{
+				["url"] = WebUtility.UrlEncode(imageUrl),
+				["output_type"] = "2",
+				["api_key"] = ApiKey,
+				["numres"] = resultCount.ToString(),
+			};
 
-		if (databases.Length == 0) pars.Add("db", "999");
-		
-		foreach(var db in databases)
-			pars.Add("dbs[]", ((int)db).ToString());
+			if (databases.Length == 0) pars.Add("db", "999");
 
-		var url = $"https://saucenao.com/search.php?{string.Join("&", pars.Select(t => $"{t.Key}={t.Value}"))}";
-		return _api.Get<Sauce>(url);
+			foreach (var db in databases)
+				pars.Add("dbs[]", ((int)db).ToString());
+
+			var url = $"https://saucenao.com/search.php?{string.Join("&", pars.Select(t => $"{t.Key}={t.Value}"))}";
+			return await _api.Get<Sauce>(url);
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError(ex, "Error occurred while looking up saucenao image: {image}", imageUrl);
+			return null;
+		}
 	}
 }
